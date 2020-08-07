966d91687bdc91e2baf9fa749cc4d57a2549b20a
mics13 <micsiu13@gmail.com>
2020-08-07 16:47:59 +0800

    rm redundant lines

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ async function getQuote() {
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
-    //console.log(data);
     // if quoteAuthor is blank, add 'Unknown'
     if (data.quoteAuthor === ''){
       authorText.innerText = 'Unknow';
